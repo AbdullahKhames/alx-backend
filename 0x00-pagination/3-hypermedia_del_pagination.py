2b933df10abcc0dbf5e6ed5093e7b4bf40e11522
AbdullahKhames <abdullahkhames96@gmail.com>
2024-01-28 22:48:32 +0200

    HATEOAS

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -39,5 +39,45 @@ class Server:
             }
         return self.__indexed_dataset
 
+    def get_page(self, index: int = 1, page_size: int = 10) -> Dict:
+        """implementing Simple pagination
+        """
+        assert (
+                isinstance(index, int)
+                and isinstance(page_size, int)
+                and index > 0
+                and page_size > 0
+        )
+        dataset = {}
+        i = 0
+        while i < page_size:
+            if index not in self.indexed_dataset().keys():
+                index += 1
+                continue
+            val = self.indexed_dataset()[index]
+            index += 1
+            if not val:
+                continue
+            dataset[index] = val
+            i += 1
+
+        return dataset
+
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
-            pass
+        """Hypermedia as the engine of application state (HATEOAS)
+            page_size: the length of the returned dataset page
+            page: the current page number
+            data: the dataset page (equivalent to return from previous task)
+            next_page: number of the next page, None if no next page
+            prev_page: number of the previous page, None if no previous page
+            total_pages: the total number of pages in the dataset as an integer
+        """
+        assert index < list(self.indexed_dataset().keys())[-1]
+        index = 0 if index is None else index
+        data = self.get_page(index, page_size)
+        return {
+            'index': index,
+            'next_index': list(data.keys())[-1],
+            'page_size': len(data),
+            'data': data,
+        }
