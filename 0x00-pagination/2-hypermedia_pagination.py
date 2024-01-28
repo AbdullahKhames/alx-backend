0650f2e2fa5492956be71a236480871092049421
AbdullahKhames <abdullahkhames96@gmail.com>
2024-01-28 22:11:58 +0200

    HATEOAS

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -52,12 +52,13 @@ class Server:
         """
         total_items = len(self.dataset())
         total_pages = total_items / page_size
+        data = self.get_page(page, page_size)
         return {
-            'page_size': page_size,
+            'page_size': len(data),
             'page': page,
-            'data': self.get_page(page, page_size),
-            'next_page': None if page == total_pages else page + 1,
-            'prev_page': None if page == 1 else page - 1,
+            'data': data,
+            'next_page': None if page >= total_pages else page + 1,
+            'prev_page': None if page <= 0 else page - 1,
             'total_pages': total_pages
         }
 
